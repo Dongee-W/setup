33b8af96550f35220effb5cbb4e98677d29785a0
sctech <atfrontier@hotmail.com.tw>
2013-08-11 17:06:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/sctech/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
